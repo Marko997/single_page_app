cc2a427e3a848fce822851ae851d89c79c62d0fb
Marko Simijonovic <marko.simijonovic.16@singimail.rs>
2020-08-05 22:13:32 +0200

    initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,174 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Layout, Input, Row, Col, Card, Tag, Modal, Typography } from 'antd';
+import 'antd/dist/antd.css';
+
+const API_KEY = '3f55ab13';
+const { Header, Content } = Layout;
+const { Search } = Input;
+const { Meta } = Card;
+const TextTitle = Typography.Title;
+
+
+const SearchBox = ({searchHandler}) => {
+    return (
+        <Row>
+            <Col span={12} offset={6}>
+                <Search
+                    placeholder="Unesite naziv filma, godinu, zanr, glumca ili sadrzaj.."
+                    enterButton="Pretraga"
+                    size="large"
+                    onSearch={value => searchHandler(value)}
+                />
+            </Col>
+        </Row>
+    )
+}
+
+const ColCardBox = ({Title, imdbID, Poster, Type, ShowDetail, DetailRequest, ActivateModal}) => {
+
+    const clickHandler = () => {
+
+        ActivateModal(true);
+        DetailRequest(true);
+
+        fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`)
+        .then(resp => resp)
+        .then(resp => resp.json())
+        .then(response => {
+            DetailRequest(false);
+            ShowDetail(response);
+        })
+    }
+
+    return (
+        <Col style={{margin: '20px'}} className="gutter-row" span={4}>
+            <div className="gutter-box">
+                <Card
+                    style={{ width: 200 }}
+                    cover={
+                        <img
+                            alt={Title}
+                            src={Poster}
+                        />
+                    }
+                    onClick={() => clickHandler()}
+                >
+                    <Meta
+                            title={Title}
+                            description={false}
+                    />
+                    <Row style={{marginTop: '10px'}} className="gutter-row">
+                        <Col>
+                            <Tag color="red">{Type}</Tag>
+                        </Col>
+                    </Row>
+                </Card>
+            </div>
+        </Col>
+    )
+}
+
+const MovieDetail = ({Title, Poster, imdbRating, Rated, Runtime, Genre, Plot}) => {
+    return (
+        <Row>
+            <Col span={11}>
+                <img 
+                    src={Poster} 
+                    alt={Title} 
+                />
+            </Col>
+            <Col span={13}>
+                <Row>
+                    <Col span={21}>
+                        <TextTitle level={4}>{Title}</TextTitle></Col>
+                    <Col span={3} style={{textAlign:'right'}}>
+                        <TextTitle level={4}><span style={{color: '#41A8F8'}}>{imdbRating}</span></TextTitle>
+                    </Col>
+                </Row>
+                <Row style={{marginBottom: '20px'}}>
+                    <Col>
+                        <Tag>{Rated}</Tag> 
+                        <Tag>{Runtime}</Tag> 
+                        <Tag>{Genre}</Tag>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>{Plot}</Col>
+                </Row>
+            </Col>
+        </Row>
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState(null);
+    const [q, setQuery] = useState(2018);
+    const [activateModal, setActivateModal] = useState(false);
+    const [detail, setShowDetail] = useState(false);
+    const [detailRequest, setDetailRequest] = useState(false);
+
+
+    useEffect(() => {
+
+        setData(null);
+
+        fetch(`http://www.omdbapi.com/?s=${q}&apikey=${API_KEY}`)
+        .then(resp => resp)
+        .then(resp => resp.json())
+        .then(response => {
+            if (response.Response !== 'False') {
+                setData(response.Search)
+            }
+
+        })
+
+    }, [q]);
+
+    
+    return (
+        <div className="App">
+            <Layout className="layout">
+                <Header>
+                    <div style={{ textAlign: 'center'}}>
+                        <TextTitle style={{color: '#ffffff', marginTop: '10px'}} level={2}>Single page app</TextTitle>
+                    </div>
+                </Header>
+                <Content style={{ padding: '50px' }}>
+                    <div style={{ background: '#fff', padding: 30, minHeight: 150 }}>
+                        <SearchBox searchHandler={setQuery} />
+                        <br />
+                        
+                        <Row gutter={16} type="flex" justify="center">
+
+                            
+                            { data !== null && data.length > 0 && data.map((result, index) => (
+                                <ColCardBox 
+                                    ShowDetail={setShowDetail} 
+                                    DetailRequest={setDetailRequest}
+                                    ActivateModal={setActivateModal}
+                                    key={index} 
+                                    {...result} 
+                                />
+                            ))}
+                        </Row>
+                    </div>
+                    <Modal
+                        title='Detail'
+                        centered
+                        visible={activateModal}
+                        onCancel={() => setActivateModal(false)}
+                        width={800}
+                        >
+                        { detailRequest  === false &&
+                            (<MovieDetail {...detail} />) 
+                        }
+                    </Modal>
+                </Content>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
